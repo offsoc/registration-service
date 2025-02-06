5623319dade8ae522638acd96d4bedd8a4d06bcc
Katherine Yen <katherine@signal.org>
2025-02-06 09:25:57 -0500

    Revert "Revert "Update Infobip SDK to 5.1.0""
    This reverts commit dff9a446ef98c302094d61a671ee7f770f832074.

--- src/main/java/org/signal/registration/sender/infobip/classic/InfobipSmsSender.java
+++ src/main/java/org/signal/registration/sender/infobip/classic/InfobipSmsSender.java
@@ -10,11 +10,12 @@ import com.google.i18n.phonenumbers.Phonenumber;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.infobip.ApiException;
 import com.infobip.api.SmsApi;
-import com.infobip.model.SmsAdvancedTextualRequest;
 import com.infobip.model.SmsDestination;
+import com.infobip.model.SmsMessage;
+import com.infobip.model.SmsRequest;
 import com.infobip.model.SmsResponse;
 import com.infobip.model.SmsResponseDetails;
-import com.infobip.model.SmsTextualMessage;
+import com.infobip.model.SmsTextContent;
 import io.micrometer.core.instrument.Timer;
 import io.micronaut.scheduling.TaskExecutors;
 import jakarta.inject.Named;
@@ -112,21 +113,21 @@ public class InfobipSmsSender implements VerificationCodeSender {
     final String e164 = PhoneNumberUtil.getInstance().format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.E164);
 
     final String verificationCode = verificationCodeGenerator.generateVerificationCode();
-    final String body = verificationSmsBodyProvider.getVerificationBody(phoneNumber, clientType, verificationCode, languageRanges);
+    final SmsTextContent content = new SmsTextContent().text(verificationSmsBodyProvider.getVerificationBody(phoneNumber, clientType, verificationCode, languageRanges));
 
-    final SmsTextualMessage smsMessage = new SmsTextualMessage()
-        .from(senderIdSelector.getSenderId(phoneNumber))
+    final SmsMessage smsMessage = new SmsMessage()
+        .sender(senderIdSelector.getSenderId(phoneNumber))
         .addDestinationsItem(new SmsDestination().to(e164))
-        .text(body);
+        .content(content);
 
-    final SmsAdvancedTextualRequest smsMessageRequest = new SmsAdvancedTextualRequest()
+    final SmsRequest smsMessageRequest = new SmsRequest()
         .messages(List.of(smsMessage));
 
     final Timer.Sample sample = Timer.start();
 
     return CompletableFuture.supplyAsync(() -> {
       try {
-        final SmsResponse smsResponse = smsApiClient.sendSmsMessage(smsMessageRequest).execute();
+        final SmsResponse smsResponse = smsApiClient.sendSmsMessages(smsMessageRequest).execute();
         final String messageId = checkSenderRejectedAndExtractMessageId(smsResponse.getMessages());
 
         return new AttemptData(Optional.of(messageId),
